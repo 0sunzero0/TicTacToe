d2773467c631a411bd8d37ba42fefd5d59f60065
hyeyeongKim <R03160719@github.com>
2018-06-20 11:18:47 +0900

    modified Computer.java in hyeyeongTest

--- TicTacToe/src/tictactoe/Computer.java
+++ TicTacToe/src/tictactoe/Computer.java
@@ -1,3 +1,6 @@
+/*Computer class는 인공지능으로써 자신의 mark를 표시할 위치를 결정하는 class입니다.
+이 클래스는 레벨이 여러 개로 나뉘어 구현되어 있고 레벨을 선택할 수 있습니다.
+*/
 .PHONY: all clean testckage tictactoe;
 import java. util.Scanner;
 import java.util.Random;
@@ -15,7 +18,8 @@ public class Computer extends Game implements Runnable {
 	//	this.level = level;
 	//}
 	//메인말고 메소드로 만듬.
-
+	
+	// 레벨 입력
 	public void inputlevel() {
 		Scanner scanner = new Scanner(System.in);
 		int level;
@@ -58,6 +62,7 @@ public class Computer extends Game implements Runnable {
 
 	}
 
+	// 레벨에 따른 컴퓨터 실행
 	public void run(int[][] map) {
 		if (level == 1)
 			randomInput(map);
@@ -66,7 +71,9 @@ public class Computer extends Game implements Runnable {
 		else if (level == 3)
 			computerInput(map);
 	}
-
+	
+	// Computer(medium)는 Computer가 recursively 하게 경우의 수를 다 계산하여서 가중치를 둡니다.
+	//  가장 효율적인 곳을 찾아 위치를 결정합니다.
 	private void medium(int[][] map) {
 		int[][] copyMap = new int[3][3];
 		int index = -1;
@@ -117,7 +124,8 @@ public class Computer extends Game implements Runnable {
 
 		return calculate(map, index + 1, turn);
 	}
-
+	
+	// Computer(easy)는 Computer가 랜덤으로 위치를 결정합니다.
 	private void randomInput(int[][] map) {
 		int n = random.nextInt(9);
 		while(map[n / 3][n % 3] != 0)
@@ -181,6 +189,11 @@ public class Computer extends Game implements Runnable {
 
 	}
 
+	/* Computer(hard)는 Computer가 주어진 상황에서 어떤 경우의 수가 있는지 다 검토합니다.
+	 * 1순위 다음 수를 두어서 경기를 끝낼 수 있는 경우
+	 * 2순위 지금 막지 않으면 상대방이 경기를 끝나게 되어 지는 경우
+	 * 3순위 먼저 가장 중간 그 다음으로 꼭짓점 그다음으로 랜덤 
+	 * 3가지 경우를 살피는데 1순위 부분을 조건문을 제일 앞에 둠으로써 먼저 체크하고 수행합니다. */
 	public void computerInput(int[][] map){
 		if(chkChance(map) != 9 && chkOvrlap(map, chkChance(map)/3, chkChance(map)%3) == false){
 			map[chkChance(map)/3][chkChance(map)%3] = -1;
